9ebe065d81c1f962dc72b038472728f31fe69d0e
no-you-remon <89887507+no-you-remon@users.noreply.github.com>
2021-09-15 14:27:38 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,10 +18,10 @@ const app = Vue.createApp({
       */
       correctAnswer: {
         stage1: {
-          q1: 'あああ',
+          q1: '0344',
         },
         stage2: {
-          q1: 'いいい',
+          q1: 'よんかい',
           //q2: 'えええ',
           // q3: 'おおお'
         },
